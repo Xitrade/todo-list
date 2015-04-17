9c6327ab35247bd0fdb0dc8733163ed73c531a01
Ulises Gomez - Jaramillo <ulisesgomezjaramillo@gmail.com>
2015-04-16 21:37:06 -0700

    yep 11

--- includes/add-task.php
+++ includes/add-task.php
@@ -3,4 +3,10 @@
 	$date = date('Y-m-d');
 	$time = time('H:i:s');
 
+	include ('connect.php');
+
+	$mysqli = new mysqli('localhost', 'root', 'root', 'todo');
+	$mysqli -> query("INSERT INTO tasks VALUES (', '$task', '$date', '$time')");
+
+	$query = "SELECT * FROM tasks WHERE task ='$task' and date = '$date' and time = '$time'";
 ?>
